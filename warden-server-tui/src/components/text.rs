8363170bdb872f351af5038c4e25e3a8d3876098
Ahmed Rashrash <ahmed.rashrash@gmail.com>
2026-07-18 15:05:42 -0400

    replaced Text with Line for single line text

--- warden-server-tui/src/components/text.rs
+++ warden-server-tui/src/components/text.rs
@@ -2,7 +2,7 @@ use ratatui::{
     buffer::Buffer,
     layout::{Offset, Rect},
     style::Style,
-    text::{Line, Text},
+    text::Line,
     widgets::Widget,
 };
 
@@ -18,13 +18,10 @@ impl StyledLabelledText {
     const LABEL_DELIM: &'static str = ": ";
 
     pub fn render(&mut self, area: Rect, buf: &mut Buffer) {
-        let mut host_label = Text::default();
-        host_label
-            .push_line(Line::from(self.label.clone() + Self::LABEL_DELIM).style(self.label_style));
+        let host_label = Line::from(self.label.clone() + Self::LABEL_DELIM).style(self.label_style);
         host_label.render(area.offset(Offset { x: 0, y: 0 }), buf);
 
-        let mut text = Text::default();
-        text.push_line(Line::from(self.value.clone()).style(self.value_style));
+        let text = Line::from(self.value.clone()).style(self.value_style);
         text.render(
             area.offset(Offset {
                 x: self.label.len() as i32 + Self::LABEL_DELIM.len() as i32,
